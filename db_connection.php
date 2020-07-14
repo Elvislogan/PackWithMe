31f883a6dba0ce24c5fcfc83f9aa29a4e19daa3c
Elvislogan <35869023+Elvislogan@users.noreply.github.com>
2020-07-14 13:56:50 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
     function OpenCon() {
         $hostname = "127.0.0.1";
-        $username = "user";
-        $password = "drakinho1";
+        $username = "root";
+        $password = "";
         $db = "notification";
         $conn = new mysqli($hostname, $username, $password,$db) or die("Connect failed: %s\n". mysqli_connect_error());
         
