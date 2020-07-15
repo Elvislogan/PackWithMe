edd864c3f4063283fd8188876626e02547a3f800
ElvisThunder <elvislogan1@gmail.com>
2020-07-15 14:33:48 -0700

    testing the db connection globally

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,9 @@
 <?php
     function OpenCon() {
-        $hostname = "127.0.0.1";
-        $username = "root";
-        $password = "";
-        $db = "notification";
+        $hostname = "85.10.205.173:3306";
+        $username = "thunder";
+        $password = "drakinho1";
+        $db = "notify_list";
         $conn = new mysqli($hostname, $username, $password,$db) or die("Connect failed: %s\n". mysqli_connect_error());
         
         return $conn;
